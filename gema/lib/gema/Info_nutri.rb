a689d123b46d95d9f867d1591902b1d8d9635bd5
Jiaqi Jin <alu0100981391@ull.edu.es>
2018-11-06 11:32:11 +0000

    Ejecutando el programda

---
+++ gema/lib/gema/Info_nutri.rb
@@ -0,0 +1,69 @@
+class Info_nutri
+    
+    
+    attr_accessor :nombre,:calorias, :grasas, :grasa_saturada, :hidratos_carbono, :azucares, :proteina, :sal, :porciones, :gramos
+    
+    def initialize(nombre,calorias,grasas,grasa_saturada,hidratos_carbono,azucares,proteina,sal,porciones,gramos)
+       self.nombre = nombre
+       self.calorias = calorias
+       self.grasas = grasas
+       self.grasa_saturada = grasa_saturada
+       self.hidratos_carbono = hidratos_carbono
+       self.azucares = azucares
+       self.proteina = proteina
+       self.sal = sal
+       self.porciones = porciones
+       self.gramos = gramos
+    end
+    
+    def calculate_Julios
+       (37*grasas + 37*grasa_saturada + 17*hidratos_carbono + 17*azucares + 17*proteina + 25*sal)*porciones*gramos
+    end
+    
+    def calculate_Kcal
+       (9*grasas + 9*grasa_saturada + 4*hidratos_carbono + 4*azucares + 4*proteina + 6*sal)*porciones*gramos
+    end
+    
+    def IR
+       put "8400 kJ/ 2000 kcal ,70g Grasas, 20g Ácidos grasos Saturados, 260g Hidratos de Carbono, 90g Azucares, 50g Proteínas, 6g Sal" 
+    end
+    
+    def imprimir
+        return @nombre
+    end
+    
+    def imprimirCalorias
+       return @calorias
+    end
+    
+    def imprimirGrasas
+        return @grasas
+    end
+    
+    def imprimirGrasasSaturadas
+        return @grasa_saturada
+    end
+    
+    def imprimirHidratosCarbono
+        return @hidratos_carbono
+    end
+    
+    def imprimirAzucares
+       return @azucares
+    end
+    
+    def imprimirProteinas
+       return @proteina
+    end
+    
+    def imprimirSal
+        return @sal
+    end 
+    
+ 
+    
+end
+
+
+ 
+ 
--- gema/lib/gema/version.rb
+++ gema/lib/gema/version.rb
@@ -1,3 +1,3 @@
 module Gema
-  VERSION = "1.1.0"
+  VERSION = "2.1.0"
 end
