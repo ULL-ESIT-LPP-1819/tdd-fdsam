d49c9e84243639c2be612fbd7dce28cbd83a0e71
Jiaqi Jin <alu0100981391@ull.edu.es>
2018-11-13 10:24:44 +0000

    Creando la lista

---
+++ gema/lib/gema/Lista.rb
@@ -0,0 +1,73 @@
+
+Node = Struct.new(:value, :next_, :prev)
+
+class Lista
+    
+   attr_reader :head, :tail
+   
+   def initialize(valor)
+       @head = Node.new(valor,nil,nil)
+       @tail = @head
+    end
+       
+    def insert_head(valor)
+        dummy = Node.new(valor,@head,nil)
+        @head.prev = dummy
+        @head = dummy
+    end
+    
+    
+    def insert_tail(valor)
+        dummy = Node.new(valor,nil,@tail)
+        @tail.next_ = dummy
+        @tail = dummy
+    end
+ 
+    def get_head
+        pointer = @head.value
+        @head = @head.next_
+        @head.prev = nil
+        return pointer
+    end
+    
+    def get_tail
+        pointer = @tail.value
+        @tail = tail.prev
+        @tail.next_ = nil
+        return pointer
+    end
+ 
+    def clasificar_lista
+        string = " "
+        pointer = @head
+        pointer2 = ""
+        a = Array.new
+        while(pointer != nil)do
+            if(pointer.value.imprimirSal < 3)
+                pointer2 = "Poca sal"
+            end
+            if(pointer.value.imprimirSal > 3 && pointer.value.imprimirSal < 30)
+                 pointer2 = "Medio sal"
+            end
+             if(pointer.value.imprimirSal >30)
+                pointer2 = "Muchas sal"
+            end
+            a.push(pointer.value.imprimir + " " + pointer2)
+            pointer = pointer.next_
+        end
+    a.sort
+    end
+    
+     def to_s
+        string = "("
+        pointer = @head
+        while (pointer != nil) do
+            string += "#{pointer.value.imprimirSal} gr,"
+            pointer = pointer.next_
+        end
+        string += ")"
+        return string
+    end
+    
+ 
+end
